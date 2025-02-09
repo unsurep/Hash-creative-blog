82385347fb966bb4886168f216619e25ab410d89
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-02-09 15:28:19 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    experimental: {
+        // appDir:true,
+        // serverComponentsExternalPackages: ["mongoose"],
+    },
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '/**', // Match all paths under this hostname
+            },
+        ],
+    },
+
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        };
+        return config;
+    }
+};
+
+module.exports = nextConfig;
