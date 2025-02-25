48b673a07d8a2ab3b81856b14c3e27099f9f6c83
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-02-25 14:29:31 +0100

    footer

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -7,7 +7,7 @@ const Blog = () => {
       <h1 className="font-inter font-bold text-2xl">Vol. 1</h1> 
 
       <div>
-        <iframe src="/hops.pdf" width={0}  height={0} alt='pdf-file' className="h-screen w-[20rem] md:w-[30rem] "/> 
+        <iframe src="/hops1.pdf" width={0}  height={0} alt='pdf-file' className="h-screen w-[20rem] md:w-[30rem] "/> 
       </div>
 
       {/* <h2>volume 2</h2>
